1ded66a424dfa13ad0daa182ebac6c55d2a05cdf
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-12 14:23:22 -0500

    required flash and session, app.use flash and session

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const flash = require('express-flash');
+const flash = require('express-flash');
 
-// const session = require('express-session')
-// const passport = require('passport');
 const session = require('express-session')
+// const passport = require('passport');
+// const session = require('express-session')
 const passport = require('passport');
 
-// const KnexSessionStore = require('connect-session-knex')(session);
+const KnexSessionStore = require('connect-session-knex')(session);
 const { knex } = require('./db/database');
 const routes = require('./routes/')
 
@@ -28,6 +28,17 @@ app.locals.body = {};
 
 // This is where auth will go eventually.
 app.use(express.static('public'));
+app.use(session({
+  store: new KnexSessionStore({
+    knex,
+    tablename: 'sessions'
+  }),
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || 'codedencounterskey'
+}));
+app.use(flash());
+app.use(bodyParser.urlencoded( {extended: false}))
 app.use(routes);
 app.use((req,res) => {
   res.render('index')
