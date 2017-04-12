2bb19ce3bcfd582208acd84de3270aaa48873c7a
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-12 09:28:26 -0500

    set up index.js in Routes folder

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = express();
+
+
+router.use(require('./create-profile'));
+router.use(require('./homepage'));
+router.use(require('./login'));
+router.use(require('./logout'));
+router.use(require('./myProfile'));
