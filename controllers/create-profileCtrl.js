bd72efea3d94fc7ef19a21317a3b676c8a7c24bd
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-12 12:08:43 -0500

    removed unessecary  module.exports

--- controllers/create-profileCtrl.js
+++ controllers/create-profileCtrl.js
@@ -2,21 +2,6 @@
 
 const CreateProfile = require('../models/user');
 const { knex } = require('../db/database')
-// data
-// data
-const getProfile = () =>
-  Profile().select()
-  .then( (rows) => rows )
-  .catch( (error) => {
-    throw error
-  })
-
-module.exports.show = (req, res, err) =>
-  Promise.all([getProfile()])
-  .then(([profile]) =>
-    res.render('create-profile')
-  ).catch(err)
-
 
 module.exports.create = (req, res, err) => {
   CreateProfile.forge(req.body)
