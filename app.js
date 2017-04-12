5f8029f1dfb08c44fd54de00da5a4daa267d2a81
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-12 12:08:06 -0500

    commented out flash

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const flash = require('express-flash');
-const session = require('express-session')
-const passport = require('passport');
-const KnexSessionStore = require('connect-session-knex')(session);
+// const flash = require('express-flash');
+// const session = require('express-session')
+// const passport = require('passport');
+// const KnexSessionStore = require('connect-session-knex')(session);
 const { knex } = require('./db/database');
 const routes = require('./routes/')
 
