5b387c4978b14410f79c2184a7d1a9a38a9040df
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-12 10:38:07 -0500

    completed homepage with checkboxes and correct indetation for pug

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 const router = Router();
 
 
-router.use(require('./create-profile'));
+// router.use(require('./create-profile'));
 router.use(require('./homepage'));
-router.use(require('./login'));
-router.use(require('./logout'));
-router.use(require('./myProfile'));
+// router.use(require('./login'));
+// router.use(require('./logout'));
+// router.use(require('./myProfile'));
 
 
 module.exports = router;
